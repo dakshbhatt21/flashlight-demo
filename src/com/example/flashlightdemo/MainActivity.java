09f18bf04b45900c70eae0b3afad7c208083144a
dakshbhatt21 <dakshbhatt21@gmail.com>
2014-01-28 23:50:02 +0530

    first commit

---
+++ src/com/example/flashlightdemo/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.flashlightdemo;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	
+	Button btn;
+	boolean flag = false;
+	Camera cam = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		btn = (Button)findViewById(R.id.btn);
+		
+		btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(!flag)	{
+					turnOnFlashLight();
+					flag = true;
+				}
+				else	{
+					turnOffFlashLight();
+					flag = false;
+				}
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void turnOnFlashLight() {
+	    try {
+	        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+	            cam = Camera.open();
+	            Parameters p = cam.getParameters();
+	            p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+	            cam.setParameters(p);
+	            cam.startPreview();
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        Toast.makeText(getBaseContext(), "Exception throws in turning on flashlight.", Toast.LENGTH_SHORT).show();
+	    }
+	}
+	
+	public void turnOffFlashLight() {
+	    try {
+	        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+	            cam.stopPreview();
+	            cam.release();
+	            cam = null;
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        Toast.makeText(getBaseContext(), "Exception throws in turning off flashlight.", Toast.LENGTH_SHORT).show();
+	    }
+	}
+
+}
